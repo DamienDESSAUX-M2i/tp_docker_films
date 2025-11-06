5fd71ce789812cfb9508cece9a4e59f099dfb232
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-06 14:25:50 +0100

    [wip] rename class MenuRead and add class MenuWrite.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
-from ui.menu_main import MenuMain
+from ui.menu_write import MenuWrite
+from ui.menu_read import MenuRead
 
 
 def main() -> None:
     file_path = Path("data/movies.csv")
-    menu_main = MenuMain(file_path=file_path)
+    menu_main = MenuWrite(file_path=file_path)
+    menu_read = MenuRead(file_path=file_path)
     menu_main.show()
 
 
---
+++ ui/menu_read.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+class MenuRead:
+    def __init__(self, file_path: Path):
+        self.file_path = file_path
