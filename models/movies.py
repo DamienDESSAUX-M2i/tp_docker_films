066634c92c01e15f57a0bc35cfe6b016d1ef71e0
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-06 09:20:58 +0100

    First commit.

---
+++ models/movies.py
@@ -0,0 +1,23 @@
+class Movie:
+    ID: int = 0
+
+    def __init__(self, titre: str, annee_production: int, genre: str, age_limite: int) -> None:
+        """Constructor of the class Movie.
+
+        Args:
+            titre (str): Title of the movie.
+            annee_production (int): Year of production of the movie.
+            genre (str): Movie type.
+            age_limite (int): Age limit for the movie.
+        """
+        self.titre: str = titre
+        self.annee_production: int = annee_production
+        self.genre: str = genre
+        self.age_limite: int = age_limite
+    
+    def __str__(self) -> None:
+        """Display attributs and values of the class."""
+        infos: list[str] = ["=== Infos ==="]
+        for attribut, value in self.__dict__.items():
+            infos.append(f"{attribut} : {value}")
+        print("/n".join(infos))
