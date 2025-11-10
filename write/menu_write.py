f2f2a17bd7ba1eeab495a53f016443048f8ab149
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-10 08:30:00 +0100

    [bigfix] Empty database case.

--- write/menu_write.py
+++ write/menu_write.py
@@ -38,46 +38,52 @@ class MenuWrite(Menu):
 
     def modify_movie(self) -> None:
         print("=== Modifier un film ===")
-        id_movie = self.show_id(
-            born_inf=1,
-            born_sup=Movie.ID,
-            msg="ID : ",
-            msg_error=f"L'identifiant doit être un nombre entier compris entre 1 et {Movie.ID}.",
-        )
-        titre: str = self.show_title(msg="Titre : ")
-        annee_production: int = self.show_annee_production(
-            msg="Année de production : ",
-            msg_error="L'année de production doit être un nombre entier supérieur ou égal à 0.",
-            born_inf=0,
-        )
-        genre: Genre = self.show_genre(
-            msg="Genre : ",
-            msg_error=f"Le genre n'est pas valide.\nLa liste des genres valide est : {', '.join(Genre.get_values())}.",
-        )
-        age_limit: int = self.show_age_limit(
-            msg="Age limite : ",
-            msg_error="L'âge doit être un nombre entier positif.",
-        )
-        manip_data.mofify_movie(
-            file_path=self.file_path,
-            id_movie=id_movie,
-            titre=titre,
-            annee_production=annee_production,
-            genre=genre,
-            age_limite=age_limit,
-        )
-        print("Film correctement modifié !")
+        if Movie.ID == 0:
+            print("Il n'y a pas de film dans la base de données.")
+        else:
+            id_movie = self.show_id(
+                born_inf=1,
+                born_sup=Movie.ID,
+                msg="ID : ",
+                msg_error=f"L'identifiant doit être un nombre entier compris entre 1 et {Movie.ID}.",
+            )
+            titre: str = self.show_title(msg="Titre : ")
+            annee_production: int = self.show_annee_production(
+                msg="Année de production : ",
+                msg_error="L'année de production doit être un nombre entier supérieur ou égal à 0.",
+                born_inf=0,
+            )
+            genre: Genre = self.show_genre(
+                msg="Genre : ",
+                msg_error=f"Le genre n'est pas valide.\nLa liste des genres valide est : {', '.join(Genre.get_values())}.",
+            )
+            age_limit: int = self.show_age_limit(
+                msg="Age limite : ",
+                msg_error="L'âge doit être un nombre entier positif.",
+            )
+            manip_data.mofify_movie(
+                file_path=self.file_path,
+                id_movie=id_movie,
+                titre=titre,
+                annee_production=annee_production,
+                genre=genre,
+                age_limite=age_limit,
+            )
+            print("Film correctement modifié !")
 
     def remove_movie(self) -> None:
         print("=== Supprimer un film ===")
-        id_movie: int = self.show_id(
-            born_inf=1,
-            born_sup=Movie.ID,
-            msg="ID : ",
-            msg_error=f"L'identifiant doit être un nombre entier compris entre 1 et {Movie.ID}.",
-        )
-        manip_data.remove_movie(file_path=self.file_path, id_movie=id_movie)
-        print("Film correctement supprimé !")
+        if Movie.ID == 0:
+            print("Il n'y a pas de film dans la base de données.")
+        else:
+            id_movie: int = self.show_id(
+                born_inf=1,
+                born_sup=Movie.ID,
+                msg="ID : ",
+                msg_error=f"L'identifiant doit être un nombre entier compris entre 1 et {Movie.ID}.",
+            )
+            manip_data.remove_movie(file_path=self.file_path, id_movie=id_movie)
+            print("Film correctement supprimé !")
 
     def show(self):
         while True:
